aa945a6e9ae7d4cb9870d5143e7d585a02481b8e
Nico <nico@Nicos-MacBook-Pro.local>
2019-02-25 18:14:46 +0200

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+
+var config = {
+    apiKey: "AIzaSyCX5s1rXxnTBQMskf1wumAzhZiuaH2FgjI",
+    authDomain: "react-slack-clone-c2eed.firebaseapp.com",
+    databaseURL: "https://react-slack-clone-c2eed.firebaseio.com",
+    projectId: "react-slack-clone-c2eed",
+    storageBucket: "react-slack-clone-c2eed.appspot.com",
+    messagingSenderId: "923754099357"
+  };
+  firebase.initializeApp(config);
+
+export default firebase;
