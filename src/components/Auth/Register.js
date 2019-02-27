7a9254793b8fe786eab16d9582a4364e84e4de56
Nico <nico@Nicos-MacBook-Pro.local>
2019-02-27 15:30:12 +0200

    add name and avatar to users

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,5 +1,6 @@
 import React from "react";
 import firebase from "../../firebase";
+import md5 from "md5";
 import {
   Grid,
   Form,
@@ -18,7 +19,8 @@ class Register extends React.Component {
     password: "",
     passwordConfirmation: "",
     errors: [],
-    loading: false
+    loading: false,
+    userRef: firebase.database().ref('users')
   };
 
   isFormValid = () => {
@@ -73,7 +75,17 @@ class Register extends React.Component {
         .createUserWithEmailAndPassword(this.state.email, this.state.password)
         .then(createdUser => {
           console.log(createdUser);
-          this.setState({ loading: false });
+          createdUser.user.updateProfile({
+            displayName: this.state.username,
+            photoURL: `http://gravatar.com./avatar/${md5(
+              createdUser.user.email
+            )}?d=identicon`
+          })
+          .then(() => {
+            this.saveUser(createUser).then(() => {
+              console.log('user saved');
+            })
+          })
         })
         .catch(err => {
           console.error(err);
@@ -85,13 +97,18 @@ class Register extends React.Component {
     }
   };
 
+saveUser = createdUser => {
+  return this.state.userRef.child(createUser.user.uid).set({
+    name: createdUser.user.displayName,
+    avatar: createdUser.user.photoURL
+  });
+};
+
   handleInputError = (errors, inputName) => {
-    return errors.some(error =>
-      error.message.toLowerCase().includes(inputName)
-     )
-      ? "error" 
-      : ""
-  }
+    return errors.some(error => error.message.toLowerCase().includes(inputName))
+      ? "error"
+      : "";
+  };
 
   render() {
     const {
@@ -131,7 +148,7 @@ class Register extends React.Component {
                 placeholder="Email Address"
                 onChange={this.handleChange}
                 value={email}
-                className={this.handleInputError(errors, 'email')}
+                className={this.handleInputError(errors, "email")}
                 type="email"
               />
 
@@ -143,7 +160,7 @@ class Register extends React.Component {
                 placeholder="Password"
                 onChange={this.handleChange}
                 value={password}
-                className={this.handleInputError(errors, 'password')}
+                className={this.handleInputError(errors, "password")}
                 type="password"
               />
 
@@ -155,7 +172,7 @@ class Register extends React.Component {
                 placeholder="Password Confirmation"
                 onChange={this.handleChange}
                 value={passwordConfirmation}
-                className={this.handleInputError(errors, 'password')}
+                className={this.handleInputError(errors, "password")}
                 type="password"
               />
 
