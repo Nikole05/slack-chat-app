fbcc97c0f86b224e7a616308036a8d6c003064d2
Nico <nico@Nicos-MacBook-Pro.local>
2019-02-28 22:31:20 +0200

    add firebase to index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
