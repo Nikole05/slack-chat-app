2d338786384dd322098b27c28b45f5467f82f3ac
Nico <nico@Nicos-MacBook-Pro.local>
2019-03-01 22:29:54 +0200

    ScaffoldApp Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, GridColumn } from 'semantic-ui-react';
 import './App.css';
 
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+
+
 const App = () => (
-  <Grid>
-    <ColorPanel />
-    <SidePanel />
-    <Messages />
-    <MetaPanel />
+  <Grid columns="equal" className="app" style={{ background: '#eee' }} >
+      <ColorPanel />
+      <SidePanel />
+
+    <GridColumn style={{ marginLeft: 320 }}>
+      <Messages />
+    </GridColumn>
+
+    <GridColumn width={4}>
+      <MetaPanel />
+    </GridColumn>   
   </Grid>
 )
 
