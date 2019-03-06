1e2c61338c4c25fa54090bb785a6a40ced2cbd70
Nico <nico@Nicos-MacBook-Pro.local>
2019-03-06 22:44:53 +0200

    put channel on global state, add channel_reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import * as actionTypes from "./types";
 
 
+/* User Actions */
 export const setUser = user => {
     return {
         type: actionTypes.SET_USER,
@@ -13,5 +14,15 @@ export const setUser = user => {
 export const clearUser = () => {
     return {
         type: actionTypes.CLEAR_USER
+    };
+};
+
+/* Channel Actions */
+export const setCurrentChannel = channel => {
+    return {
+        type: actionTypes.SET_CURRENT_CHANNEL,
+        payload: {
+            currentChannel: channel
+        }
     }
 }
