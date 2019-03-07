1c626e8d87de3471fea777d91b6f2a4b2f03fc8a
Nico <nico@Nicos-MacBook-Pro.local>
2019-03-07 21:09:44 +0200

    add segment and comment to messages component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Segment, Comment } from "semantic-ui-react";
 
 class Messages extends React.Component {
     render() {
         return (
-            <div>Messages</div>
-        )
+            <React.Fragment>
+                <MessagesHeader />
+
+                <Segment>
+                    <Comment.Group className="messages">
+                    {/* Messages */}
+                    </Comment.Group>
+                </Segment>
+
+                <MessageForm />
+            </React.Fragment>
+        );
     }
 }
 
