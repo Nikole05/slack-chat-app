293820542daddc30e5ffe4b102b2a4ca41a0d127
Nico <nico@Nicos-MacBook-Pro.local>
2019-03-07 22:11:05 +0200

    add messageForm add css

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Segment, Comment } from "semantic-ui-react";
 import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
+
 
 class Messages extends React.Component {
     render() {
