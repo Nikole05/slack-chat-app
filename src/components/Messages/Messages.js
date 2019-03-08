b145800cfcd1dc685cf9bfa3e302c9d672c0a306
Nico <nico@Nicos-MacBook-Pro.local>
2019-03-08 19:46:41 +0200

    add message component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,10 +4,14 @@ import firebase from "../../firebase";
 
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
+import Message from "./Message";
+
 
 class Messages extends React.Component {
   state = {
     messagesRef: firebase.database().ref("messages"),
+    messages: [],
+    messagesLoading: true,
     channel: this.props.currentChannel,
     user: this.props.currentUser
   };
@@ -28,19 +32,34 @@ class Messages extends React.Component {
     let loadedMessages = [];
     this.state.messagesRef.child(channelId).on("child_added", snap => {
       loadedMessages.push(snap.val());
-      console.log(loadedMessages);
+     this.setState({
+         messages: loadedMessages,
+         messagesLoading:false
+     });
     });
   };
 
+  displayMessages = messages => (
+      messages.length > 0 && messages.map(message => (
+          <Message 
+          key={message.timestamp}
+          message={message}
+          user={this.state.user}
+          />
+      ))
+  )
+
   render() {
-    const { messagesRef, channel, user } = this.state;
+    const { messagesRef, messages, channel, user } = this.state;
 
     return (
       <React.Fragment>
         <MessagesHeader />
 
         <Segment>
-          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+          <Comment.Group className="messages">
+          {this.displayMessages(messages)}
+          </Comment.Group>
         </Segment>
 
         <MessageForm
