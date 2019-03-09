8ed4d391d3da06fd787adf377be7de6eb720e9b6
Nico <nico@Nicos-MacBook-Pro.local>
2019-03-09 21:59:13 +0200

    add modal to state

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -8,8 +8,13 @@ class MessageForm extends React.Component {
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     loading: false,
-    errors: []
+    errors: [],
+    modal: false
   };
+
+  openModal = () => this.setState({ modal: this });
+
+  closeModal = () => this.setState({ modal: false });
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
@@ -55,7 +60,7 @@ class MessageForm extends React.Component {
     }
   };
   render() {
-    const { errors, message, loading } = this.state;
+    const { errors, message, loading, modal } = this.state;
 
     return (
       <Segment className="message__form">
@@ -85,9 +90,14 @@ class MessageForm extends React.Component {
           />
           <Button
             color="teal"
+            onClick={this.openModal}
             content="Upload Media"
             labelPosition="right"
             icon="cloud upload"
+          />
+          <FileModal
+          modal={modal}
+          closeModal={this.closeModal}
           />
         </Button.Group>
       </Segment>
